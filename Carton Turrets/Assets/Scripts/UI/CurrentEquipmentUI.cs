e384476ab9a84c80d4c873d9905797ac74908265
Mitchell Greene <mitchell.lee.greene@gmail.com>
2022-09-06 19:40:31 -0400

    upgrade ui

--- Carton Turrets/Assets/Scripts/UI/CurrentEquipmentUI.cs
+++ Carton Turrets/Assets/Scripts/UI/CurrentEquipmentUI.cs
@@ -10,10 +10,17 @@ public class CurrentEquipmentUI : MonoBehaviour
 
     [SerializeField]SpriteRenderer[] _buttonSpritesArray;
     [SerializeField]TMP_Text[] _buttonTextArray;
+    [SerializeField]GameObject[] _buttonCoverArray;
+    [SerializeField]float _startingCoverScale;
 
     public void UpdateUpgradeUI(int slot, Sprite icon, string text)
     {
         _buttonSpritesArray[slot].sprite = icon;
         _buttonTextArray[slot].text = text;
+    }
+
+    public void UpdateUpgradeTimers(float maxTimer, int slot, float time)
+    {
+        _buttonCoverArray[slot].transform.localScale = new Vector3(_startingCoverScale, Mathf.LerpUnclamped( 0f, _startingCoverScale/maxTimer, time), _startingCoverScale);
     }
 }
